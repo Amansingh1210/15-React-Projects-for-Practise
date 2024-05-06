4c58446910bfa71e911721aa3f7baa5252bd6f01
Aman Singh <singhaman38809@gmail.com>
2024-05-06 06:51:39 +0530

    project 3 complete

--- 3-reviews/src/App.js
+++ 3-reviews/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Review from './Review'
 
 function App() {
   return (
-    <h2>reviews project</h2>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   )
 }
 
