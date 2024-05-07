3d0766fd80c5f64694cede893da0a67585de027b
Aman Singh <singhaman38809@gmail.com>
2024-05-07 08:16:01 +0530

    setup project 5

---
+++ 5-menu/src/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Categories() {
+  return (
+    <h2>Categories</h2>
+  )
+}
+
+export default Categories
---
+++ 5-menu/src/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <h2>Menu</h2>
+  )
+}
+
+export default Menu
