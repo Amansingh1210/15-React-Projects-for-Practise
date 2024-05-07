fb37a6521813a2be0d7a0a2cf6bddd847353c1ea
Aman Singh <singhaman38809@gmail.com>
2024-05-07 07:09:49 +0530

    setup project 4

---
+++ accordian/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Questions from './Questions'
+
+function App() {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
---
+++ accordian/src/Questions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+function Questions() {
+  return (
+    <div><AiOutlineMinus /></div>
+  )
+}
+
+export default Questions
