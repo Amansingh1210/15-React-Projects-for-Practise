3b2cdb28c4e62ff6e1122fba96838b89d893ea1c
Aman Singh <singhaman38809@gmail.com>
2024-05-07 21:40:59 +0530

    project 8 complete

--- 8-lorem-ipsum/src/App.js
+++ 8-lorem-ipsum/src/App.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React , {useState} from 'react'
+import data from './data'
 
 function App() {
+  const [count,setCount] = useState(0);
+  const [text,setText] = useState([]);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(amount <= 0){
+      amount = 1 
+    }
+    if(amount > 8){
+      amount = 8
+    }
+    setText(data.slice(0,amount))
+  }
   return (
-    <h1>App</h1>
+    <section className='section-center '>
+      <h3>tried of boring lorem ipsum ?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>
+          paragraphs:
+        </label>
+        <input type='number' name='amount' id='amonut' value={count} onChange={(e)=>setCount(e.target.value)}/>
+        <button type='submit' className='btn'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        {
+          text.map((item,index)=>{
+            return <p key={index}>{item}</p>
+          })
+        }
+      </article>
+    </section>
   )
 }
 
