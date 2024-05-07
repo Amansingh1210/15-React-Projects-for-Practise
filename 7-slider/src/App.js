52a669a9005ec15b7021599f4ea9116574cd2165
Aman Singh <singhaman38809@gmail.com>
2024-05-07 15:48:14 +0530

    setup project 7

---
+++ 7-slider/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
