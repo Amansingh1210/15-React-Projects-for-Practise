2d27dc5a39c5e3d08adf4e8d82e4eee48a11463f
Aman Singh <singhaman38809@gmail.com>
2024-05-09 19:10:57 +0530

    project 11 complete

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -1,10 +1,52 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
+import { links, socials } from './data'
 import logo from './logo.svg'
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
-    return <h4>navbar</h4>
+    const [showLinks, setShowlinks] = useState(false);
+    const linksContanierRef = useRef(null);
+    const linksRef = useRef(null);
+
+    useEffect(()=>{
+        const linksHeight = linksRef.current.getBoundingClientRect().height
+        if(showLinks){
+            linksContanierRef.current.style.height = `${linksHeight}px`
+        }
+        else{
+            linksContanierRef.current.style.height = '0px'
+        }
+    },[showLinks])
+    return <nav>
+        <div className='nav-center'>
+            <div className='nav-header'>
+                <img src={logo} alt='logo' />
+                <button className='nav-toggle' onClick={() => setShowlinks(!showLinks)}>
+                    <FaBars />
+                </button>
+            </div>
+            <div className='links-container' ref={linksContanierRef} >
+                <ul className='links' ref={linksRef}>
+                    {links.map((link) => {
+                        const { id, url, text } = link;
+                        return <li key={id}>
+                            <a href={url}>{text}</a>
+                        </li>
+                    })}
+
+                </ul>
+            </div>
+            <ul className='social-icons'>
+                {socials.map((social) => {
+                    const { id, url, icon } = social;
+                    return <li key={id}>
+                        <a href={url}>{icon}</a>
+                    </li>
+                })}
+
+            </ul>
+        </div>
+    </nav>
 }
 
 export default Navbar
--- 11-navbar/src/data.js
+++ 11-navbar/src/data.js
@@ -4,7 +4,7 @@ export const links = [
     {
         id: 1,
         url: '/',
-        text: 'random',
+        text: 'home',
     },
     {
         id: 2,
@@ -28,7 +28,7 @@ export const links = [
     },
 ]
 
-export const social = [
+export const socials = [
     {
         id: 1,
         url: 'https://www.twitter.com',
